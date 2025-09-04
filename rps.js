cd958b4cc63f8a9aab9ee42688ee44c69a44f51f
cdv930 <47964627+cdv930@users.noreply.github.com>
2025-09-04 16:44:11 -0700

    feat: update and add code to create working game Previous code was reworked and new code was added. This updated code provides the working console game and complies with the instructions in the lesson.

--- rps.js
+++ rps.js
@@ -1,6 +1,5 @@
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
-
   if (computerChoice == 0) {
     return "rock";
   } else if (computerChoice == 1) {
@@ -10,27 +9,73 @@ function getComputerChoice() {
   }
 }
 
+
 function getHumanChoice(){
     let humanChoice = prompt("Choose your move! Rock, Paper, or Scissors?","");
    return humanChoice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice,computerChoice){
-   humanChoice = getHumanChoice();
-   humanChoice = humanChoice.toLowerCase();
-   if (humanChoice == "rock"){
-    humanChoice ==0 ;
-   } else if (humanChoice == "paper"){
-    humanChoice == 1;
-   }else {
-     humanChoice == 2;
-   }
+function playGame(){
 
-   computerChoice = getComputerChoice();
-return computerChoice + humanChoice;
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    humanChoice = humanChoice.toLowerCase();
+    const loseMessage = "You lost, " + computerChoice + " beats " + humanChoice + "!";
+    const winMessage = "You won, " + humanChoice + " beats " + computerChoice + "!";
+    const tieMessage = "It's a tie!";
+
+    if (humanChoice == "rock") {
+      if (computerChoice == "paper") {
+        console.log(loseMessage);
+        computerScore += 1;
+      } else if (computerChoice == "scissors") {
+        console.log(winMessage);
+        humanScore += 1;
+      } else {
+        console.log(tieMessage);
+      }
+    } else if (humanChoice == "paper") {
+      if (computerChoice == "scissors") {
+        console.log(loseMessage);
+        computerScore += 1;
+      } else if (computerChoice == "rock") {
+        console.log(winMessage);
+        humanScore += 1;
+
+      } else {
+        console.log(tieMessage);
+      }
+    }
+    else {
+      if (computerChoice == "rock") {
+        console.log(loseMessage);
+        computerScore += 1;
+      } else if (computerChoice == "paper") {
+        console.log(winMessage);
+        humanScore += 1;
+      } else {
+        console.log(tieMessage);
+      }
+    }
+  }
+
+  for (let round = 0; round < 5; round++){
+    
+  playRound();
+}
+console.log("Human Score: "+ humanScore + " Computer Score: "+computerScore);
+  if (humanScore > computerScore){
+    console.log("You won!")
+  }
+  else if (humanScore == computerScore){
+    console.log("It's a tie!")
+  }
+  else{ console.log("You lost!")}
 }
 
-console.log(playRound());
+playGame();
