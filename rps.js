e6258e74e739a95d155597acaea0b92e4a0843ec
cdv930 <47964627+cdv930@users.noreply.github.com>
2025-09-18 00:28:06 -0700

    feat. Add buttons and UI for gameplay

--- rps.js
+++ rps.js
@@ -9,73 +9,103 @@ function getComputerChoice() {
   }
 }
 
-
-function getHumanChoice(){
-    let humanChoice = prompt("Choose your move! Rock, Paper, or Scissors?","");
-   return humanChoice;
+function addMessage(result) {
+  const newMessage = document.createElement("p");
+  newMessage.textContent = result;
+  messageCenter.appendChild(newMessage);
 }
 
+function playRound(humanChoice, computerChoice) {
+  const loseMessage = "You lost, " + computerChoice + " beats " + humanChoice + "!";
+  const winMessage = "You won, " + humanChoice + " beats " + computerChoice + "!";
+  const tieMessage = "It's a tie!";
 
-function playGame(){
-
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    humanChoice = humanChoice.toLowerCase();
-    const loseMessage = "You lost, " + computerChoice + " beats " + humanChoice + "!";
-    const winMessage = "You won, " + humanChoice + " beats " + computerChoice + "!";
-    const tieMessage = "It's a tie!";
-
-    if (humanChoice == "rock") {
-      if (computerChoice == "paper") {
-        console.log(loseMessage);
-        computerScore += 1;
-      } else if (computerChoice == "scissors") {
-        console.log(winMessage);
-        humanScore += 1;
-      } else {
-        console.log(tieMessage);
-      }
-    } else if (humanChoice == "paper") {
-      if (computerChoice == "scissors") {
-        console.log(loseMessage);
-        computerScore += 1;
-      } else if (computerChoice == "rock") {
-        console.log(winMessage);
-        humanScore += 1;
-
-      } else {
-        console.log(tieMessage);
-      }
+  if (humanChoice == "rock") {
+    if (computerChoice == "paper") {
+      addMessage(loseMessage);
+      updateScore("b");
+    } else if (computerChoice == "scissors") {
+      addMessage(winMessage);
+      updateScore("a");
+    } else {
+      addMessage(tieMessage);
     }
-    else {
-      if (computerChoice == "rock") {
-        console.log(loseMessage);
-        computerScore += 1;
-      } else if (computerChoice == "paper") {
-        console.log(winMessage);
-        humanScore += 1;
-      } else {
-        console.log(tieMessage);
-      }
+  } else if (humanChoice == "paper") {
+    if (computerChoice == "scissors") {
+      addMessage(loseMessage);
+      updateScore("b");
+    } else if (computerChoice == "rock") {
+      addMessage(winMessage);
+      updateScore("a");
+
+    } else {
+      addMessage(tieMessage);
     }
   }
-
-  for (let round = 0; round < 5; round++){
-    
-  playRound();
-}
-console.log("Human Score: "+ humanScore + " Computer Score: "+computerScore);
-  if (humanScore > computerScore){
-    console.log("You won!")
+  else {
+    if (computerChoice == "rock") {
+      addMessage(loseMessage);
+      updateScore("b");
+    } else if (computerChoice == "paper") {
+      addMessage(winMessage);
+      updateScore("a");
+    } else {
+      addMessage(tieMessage);
+    }
   }
-  else if (humanScore == computerScore){
-    console.log("It's a tie!")
-  }
-  else{ console.log("You lost!")}
 }
 
-playGame();
+function announceWinner(playerScore, computerScore) {
+  if (playerScore >= 5) {
+    gameWinner.textContent = "Player Wins!";
+  }
+  else if (computerScore >= 5) {
+    gameWinner.textContent = "Computer Wins!";
+  }
+}
+
+
+let playerScore= 0;
+let computerScore = 0;
+
+function updateScore(winner){
+  if(winner == "a"){
+    playerScore +=1}
+  else if(winner == "b"){
+    computerScore +=1
+  }
+  playCount.textContent = playerScore;
+  compCount.textContent = computerScore;
+  announceWinner(playerScore,computerScore);
+}
+
+
+const rockBtn = document.querySelector("#rock-button");
+const paperBtn = document.querySelector("#paper-button");
+const scissorsBtn = document.querySelector("#scissors-button");
+const messageCenter = document.querySelector("#messenger");
+const scoreComp = document.querySelector("#comp-points");
+const scorePlayer = document.querySelector("#player-points");
+const title = document.querySelector("#title");
+
+const compCount = document.createElement("h3");
+const playCount = document.createElement("h3");
+const gameWinner = document.createElement("h2");
+
+playCount.textContent = playerScore;
+compCount.textContent = computerScore;
+
+
+
+scoreComp.appendChild(compCount);
+scorePlayer.appendChild(playCount);
+title.appendChild(gameWinner);
+
+rockBtn.addEventListener("click",() =>  playRound("rock",getComputerChoice()));
+paperBtn.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissorsBtn.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+
+
+
+
+
